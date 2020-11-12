063335f695855a838fb312a9cc3bea3cc5287f15
blackholedick <sas95867@gmail.com>
2020-11-12 19:57:02 +0800

    成

--- WP_2020_01_HW01_3A713153/Form1.cs
+++ WP_2020_01_HW01_3A713153/Form1.cs
@@ -46,25 +46,3 @@ namespace WP_2020_01_HW01_3A713153
         }
     }
 }
-/*public static class MD5Extensions
-    {
-        public static string ToMD5(this string str)
-        {
-            using (var cryptoMD5 = System.Security.Cryptography.MD5.Create())
-            {
-                //將字串編碼成 UTF8 位元組陣列
-                var bytes = Encoding.UTF8.GetBytes(str);
-
-                //取得雜湊值位元組陣列
-                var hash = cryptoMD5.ComputeHash(bytes);
-
-                //取得 MD5
-                var md5 = BitConverter.ToString(hash)
-                  .Replace("-", String.Empty)
-                  .ToUpper();
-                
-
-                return md5;
-            }
-        }
-    }*/
