5baca146a271dd821a9cb1c24681e5c5e5e45798
blackholedick <sas95867@gmail.com>
2020-11-12 19:15:37 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW01_3A713153/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WP_2020_01_HW01_3A713153
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbinput = new System.Windows.Forms.TextBox();
+            this.rtboutput = new System.Windows.Forms.RichTextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbinput
+            // 
+            this.tbinput.Location = new System.Drawing.Point(143, 39);
+            this.tbinput.Margin = new System.Windows.Forms.Padding(2);
+            this.tbinput.Name = "tbinput";
+            this.tbinput.Size = new System.Drawing.Size(334, 22);
+            this.tbinput.TabIndex = 0;
+            // 
+            // rtboutput
+            // 
+            this.rtboutput.Location = new System.Drawing.Point(144, 261);
+            this.rtboutput.Margin = new System.Windows.Forms.Padding(2);
+            this.rtboutput.Name = "rtboutput";
+            this.rtboutput.Size = new System.Drawing.Size(333, 70);
+            this.rtboutput.TabIndex = 1;
+            this.rtboutput.Text = "";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGo.Location = new System.Drawing.Point(144, 89);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(333, 101);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "點我改運";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(136, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 47);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "下面是你的改運號碼";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "輸入你的名字";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "記得刻在手鍊上，你會明顯感到改運!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.rtboutput);
+            this.Controls.Add(this.tbinput);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "產生專屬於你的改運號碼";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbinput;
+        private System.Windows.Forms.RichTextBox rtboutput;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WP_2020_01_HW01_3A713153/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WP_2020_01_HW01_3A713153
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            var cryptoMD5 = System.Security.Cryptography.MD5.Create();
+            byte[] source = Encoding.Default.GetBytes(tbinput.Text);//將字串轉為Byte[]
+            byte[] crypto = cryptoMD5.ComputeHash(source);//進行MD5加密
+            //string result = Convert.ToBase64String(source);
+            var result = BitConverter.ToString(crypto).Replace("-", String.Empty).ToUpper();
+
+            /*string result = Convert.ToBase64String(crypto);//把加密後的字串從Byte[]轉為字串
+            rtboutput.Text("MD5加密:  " + result);//輸出結果*/
+
+
+
+            //var str = tbinput.Text;
+            //var md5 = str.ToMD5();
+            rtboutput.Text = result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("改變命運吧!", "想讓生活過得輕鬆嘛?",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("上吧!孩子!", "相信號碼吧!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
+public static class MD5Extensions
+    {
+        public static string ToMD5(this string str)
+        {
+            using (var cryptoMD5 = System.Security.Cryptography.MD5.Create())
+            {
+                //將字串編碼成 UTF8 位元組陣列
+                var bytes = Encoding.UTF8.GetBytes(str);
+
+                //取得雜湊值位元組陣列
+                var hash = cryptoMD5.ComputeHash(bytes);
+
+                //取得 MD5
+                var md5 = BitConverter.ToString(hash)
+                  .Replace("-", String.Empty)
+                  .ToUpper();
+                
+
+                return md5;
+            }
+        }
+    }
